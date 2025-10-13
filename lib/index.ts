bb054d4d7a80159b6432752a554e420cc3c77b2a
gaddario98 <gaddario98@gmail.com>
2025-10-13 14:43:44 +0200

    Initial commit: migrate from monorepo

---
+++ lib/index.ts
@@ -0,0 +1,9 @@
+export * from './state';
+export type AuthState = {
+    id: string;
+    accountVerified?: boolean;
+    isLogged?: boolean,
+    token?: string;
+    phoneNumber?: string;
+    email?: string;
+  };
